542b855748fb19fbc98dcba2273995b6ea3e1a69
Enzo Samojedny <enzosamojedny@gmail.com>
2024-06-23 19:08:34 -0300

    Update fn.sql

--- Functions/fn.sql
+++ Functions/fn.sql
@@ -21,4 +21,17 @@ BEGIN
     RETURN TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE());
 END$$
 
+CREATE FUNCTION `nombre_de_empleado`(var_idpersona INT ) RETURNS varchar(200)
+    READS SQL DATA
+BEGIN
+DECLARE persona_empleado VARCHAR(200);
+SELECT nombre_completo(p.nombre,p.apellido) 
+INTO persona_empleado
+FROM persona p
+INNER JOIN empleado e
+WHERE p.idpersona = var_idpersona
+LIMIT 1;
+RETURN persona_empleado;
+END$$
+
 DELIMITER ;
